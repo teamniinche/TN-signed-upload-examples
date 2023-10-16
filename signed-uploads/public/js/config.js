4f3194e7a6487c06364654ad12d8b00263221986
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-16 14:54:20 +0000

    Update config.js

--- signed-uploads/public/js/config.js
+++ signed-uploads/public/js/config.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 // Configure your cloud name, API key and API secret:
 
 const myconfig = cloudinary.config({
-  cloud_name: '<your_cloud_name>',
-  api_key: '<your_api_key>',
-  api_secret: '<your_api_secret>',
+  cloud_name: 'dapkl1ien',
+  api_key: '327969554979561',
+  api_secret: 'w0rvj0T50Jf6HHAjx1B8_oo06es',
   secure: true
 });
 
